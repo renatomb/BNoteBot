a1c46dfab7ef40977a80f05d7db966fe6886476a
Renato Monteiro Batista <r3n4t0@gmail.com>
2020-11-28 13:56:27 -0300

    Remover acentuação e tratar situação onde estava dando erro de offset.

--- validacao.php
+++ validacao.php
@@ -29,6 +29,12 @@ function formata_chaves($chaves){
    return $txt_retorno;
 }
 
+function remove_acentos($texto){
+   $search = explode(",","ç,æ,œ,á,é,í,ó,ú,à,è,ì,ò,ù,ä,ë,ï,ö,ü,ÿ,â,ê,î,ô,û,å,e,i,ø,u");
+   $replace = explode(",","c,ae,oe,a,e,i,o,u,a,e,i,o,u,a,e,i,o,u,y,a,e,i,o,u,a,e,i,o,u");
+   return str_replace($search, $replace, $texto);
+}
+
 function decode_brcode($brcode){
    $n=0;
    while($n < strlen($brcode)) {
