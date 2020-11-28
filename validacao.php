f39bb5534eff33f9875e47c0f3918fb837dc043a
Renato Monteiro Batista <r3n4t0@gmail.com>
2020-11-28 19:43:55 -0300

    Trocar emojis por dois espacos.

--- validacao.php
+++ validacao.php
@@ -40,7 +40,7 @@ function remove_emoji($string){
    \xF0[\x90-\xBF][\x80-\xBF]{2}      # planes 1-3
  | [\xF1-\xF3][\x80-\xBF]{3}          # planes 4-15
  | \xF4[\x80-\x8F][\x80-\xBF]{2}      # plane 16
-)%xs', ' ', $string);      
+)%xs', '  ', $string);      
 }
 function decode_brcode($brcode){
    $n=0;
