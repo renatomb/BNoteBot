02a51a2d5337f02c6a399b028eb51e1ba4a2ab8f
Renato Monteiro Batista <r3n4t0@gmail.com>
2020-11-28 23:14:21 -0300

    Alteracao no arquivo de mensagens.

--- mensagens.php
+++ mensagens.php
@@ -17,57 +17,5 @@ Este bot não se comunica com o sistema do BACEN, logo as chaves já devem ter s
 
 Bot criado por @r3n4t0, gostou? envie um pix! :) 
 8d28bec9-327f-41ee-ad6b-456318c5cce1");
-/*define(ADICIONAR,"Adicionar chave 🔑 pix");
-define(SALVO,"Ver chaves salvas");
-define(FEEDBACK,"Feedback 📫");
-define(INFORMACOES,"Informacoes: ");
-define(CONFIGURAR,"Configurações 🔧");
-define(CONTRIBUA,"Colabore com o bot");
-define(GITHUB,"Código fonte");
-define(M_CONTRIBUA,array(array(
-   "text" => $lang['vote'],
-   "url" => "https://telegram.me/storebot?start=meupix_bot"), array(
-   "text" => "PicPay 💳",
-   "url" => "http://picpay.me/renatomb")
-//            , array("text" => "Bitcoin 💰","url" => "https://paste.ubuntu.com/24299810/")
-));
-define(M_GITHUB,array(array(
-   "text" => $lang['github'],
-   "url" => "https://github.com/renatomb/meuPixBot")));
-define(COMOCOLABORAR,"Você pode colaborar com o bot das seguintes formas.");
-$lang['info'] = "Info ℹ";
-$lang['vote'] = "Votar ⭐";
-$lang['cancel'] = "Cancelar ❌";
-$lang['addmemotext'] = "Informe a chave pix 🔑:";
-$lang['cancelled'] = "Cancelado ❌";
-$lang['saved'] = "Cahve 🔑 pix salva! ✅";
-$lang['next'] = "Próximo ➡";
-$lang['back'] = "⬅ Voltar";
-$lang['datememo'] = "Data: ";
-$lang['delete'] = "Apagar ❌";
-$lang['no'] = "Não ✖";
-$lang['deleted'] = "Apagado! ✅";
-$lang['nomemo'] = "Nenhuma nota encontrada 😫";
-$lang['end'] = "Fim 🔚";
-$lang['inlinemode'] = "Modo integrado 💬";
-$lang['deleteallnote'] = "Apagar todas as notas ❌";
-$lang['settingsinline'] = "Configurações";
-$lang['settingstextinline'] = "💬 - Modo integrado
-🔧 - Você pode alterar as configurações do modo integrado através desses botões:";
-$lang['invertmemodata'] = "Inverter data e nota: ";
-$lang['enabled'] = "Habilitado";
-$lang['disabled'] = "Desabilitado";
-$lang['askdeleteallnote'] = "Apagar todas as notas?❌";
-$lang['messagenovalid'] = "Mensagem inválida. Se você possui problemas com o bot digite /start para reiniciar.";
-$lang['infomsg'] = "Com esse bot você pode salvar notas e usá-las através do modo integrado!
-📝 - Salve uma nota e, no campo do chat, escreva @BNoteBot (ou selecione-o através da lista). Uma lista com todas as suas notas salvas no bot estarão prontas para serem enviadas!";
-$lang['subchannel'] = "Entrar no canal";
-$lang['votemsg'] = "Se você deseja me ajudar, vote no meu bot na @StoreBot.";
-$lang['feedback'] = "Feedback 📫";
-$lang['feedbacktext'] = "📫 - Se esse bot contém problemas ou se você deseja sugerir algo, mande-me uma mensagem por chat (@franci22) ou envie uma mensagem agora:";
-$lang['thanksfeedback'] = "Obrigado pelo seu feedback! ✅";
-$lang['onlytxt'] = "O bot suporta apenas texto por enquanto.";
-$lang['remindertext'] = "Reminder🕒\nSão las ";*/
-
 
 ?>
